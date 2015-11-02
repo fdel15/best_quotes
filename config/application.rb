45489069e39998cb900e416c18d61cd1b928982f
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 15:08:22 -0500

    auto load controller classes

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require "rulers2"
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
-require "quotes_controller"
 
 module BestQuotes
   class Application < Rulers2::Application
