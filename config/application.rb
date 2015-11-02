678149486840f93828b68e37506f521c6e4717fe
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 12:50:11 -0500

    add quotes controller to config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require "rulers2"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require "quotes_controller"
+
 module BestQuotes
   class Application < Rulers2::Application
   end
