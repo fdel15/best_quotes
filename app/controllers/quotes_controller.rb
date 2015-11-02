ea40acc17530f9f3b5ea67dfbe1ee9f2b26ba3f3
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 17:00:46 -0500

    create single quote view from db

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,9 @@ class QuotesController < Rulers2::Controller
     @noun = "winking"
     render :a_quote
   end
+
+  def quote_1
+    quote_1 = Rulers2::Model::FileModel.find(1)
+    render :quote_1, :obj => quote_1
+  end
 end
