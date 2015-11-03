e02d3acb1bb9a41987c478dbc001379f90d4bf85
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-03 16:16:06 -0500

    update sqlite_test script to include find method

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -11,3 +11,9 @@ mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!
 mt = MyTable.create "title" => "I saw it!"
 
 puts "Count: #{MyTable.count}"
+
+top_id = mt["id"].to_i
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id["title"]}."
+end
