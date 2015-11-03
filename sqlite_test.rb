3531edb8279985ee6714b252821c515f5818340a
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-03 16:26:22 -0500

    add save method to sqlite_test

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -6,9 +6,13 @@ STDERR.puts MyTable.schema.inspect
 
 # Create row
 
-mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!"
+mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "This is an update!"
+mt.save!
 
-mt = MyTable.create "title" => "I saw it!"
+# mt2 = MyTable.find mt["id"]
+
+# puts "Title: #{mt2["title"]}"
 
 puts "Count: #{MyTable.count}"
 
