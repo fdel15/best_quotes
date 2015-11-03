41290ccba24f27226d1cf6bab958a5359cc5125d
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-03 16:06:14 -0500

    create two rows in MyTable

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -3,3 +3,11 @@ require "rulers2/sqlite_model"
 
 class MyTable < Rulers2::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
+
+# Create row
+
+mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!"
+
+mt = MyTable.create "title" => "I saw it!"
+
+puts "Count: #{MyTable.count}"
